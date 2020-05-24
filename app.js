f92aab24f50dcaf58e989cd928fe1434404c3414
by0456 <58273338+by0456@users.noreply.github.com>
2020-05-24 22:38:06 +0800

    commit

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const VerifySession = require('./modules/verifySession');
 app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token, x-refresh-token, _id");
 
