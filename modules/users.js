02a414d013c5fded6373d0335493042d76928946
by0456 <58273338+by0456@users.noreply.github.com>
2020-05-25 06:41:30 +0800

    commit

--- modules/users.js
+++ modules/users.js
@@ -11,6 +11,8 @@ exports.checkUserExists = (req, res) => {
 
     let email = req.body.email;
     let password = req.body.password;
+    console.log(email);
+    console.log(password);
     User.findByCredentials(email, password).then((user) => {
         res.send(user);
     }).catch((e) => {
